ae68a395715dfeaf3e83c7149b2f92d2489438ec
KellyPrietop <kellyp84@hotmail.com>
2022-09-07 19:04:59 -0500

    Menu Equipo

--- Torneo.App.Consola/Program.cs
+++ Torneo.App.Consola/Program.cs
@@ -7,12 +7,14 @@ namespace Torneo.App.Consola
     {
         private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
         private static IRepositorioDT _repoDT = new RepositorioDT();
+        private static IRepositorioEquipo _repoEquipo = new RepositorioEquipo();
         static void Main(string[] args)
         {
          int opcion = 0;
             do{
                 Console.WriteLine("1.Insertar Municipio");
                 Console.WriteLine("2.Insertar Director Tecnico");
+                Console.WriteLine("3.Insertar Equipo");
                 Console.WriteLine("0.Salir");
                 opcion = Int32.Parse(Console.ReadLine());
                 switch (opcion)
@@ -23,6 +25,9 @@ namespace Torneo.App.Consola
                     case 2 :
                         AddDT();
                         break;
+                    case 3:
+                        AddEquipo();
+                        break;    
                 }
 
             } while (opcion !=0) ;
@@ -55,5 +60,26 @@ namespace Torneo.App.Consola
                 };
                 _repoDT.AddDT(directorTecnico);
             }
+
+            private static void AddEquipo()
+            {
+                Console.WriteLine("Esciba el nombre del Equipo");
+                string nombre =Console.ReadLine();
+                Console.WriteLine("Esciba el id del Municipio");
+                int idMunicipio =Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Esciba el id del DT");
+                int idDT = Int32.Parse(Console.ReadLine());
+
+
+                var equipo = new Equipo
+                {
+                    Nombre = nombre,
+                };
+                _repoEquipo.AddEquipo(equipo, idMunicipio,idDT);
+            }
+  
+
+
+
     }
 }
