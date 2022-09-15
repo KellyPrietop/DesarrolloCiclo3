47af4bb819317621a92aa4e81215bd734ea1025f
KellyPrietop <kellyp84@hotmail.com>
2022-09-14 21:46:01 -0500

    listar Municipios

--- Torneo.App.Frontend/Program.cs
+++ Torneo.App.Frontend/Program.cs
@@ -1,7 +1,13 @@
+using Torneo.App.Persistencia;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSingleton<IRepositorioMunicipio, RepositorioMunicipio>();
+
+
 
 var app = builder.Build();
 
